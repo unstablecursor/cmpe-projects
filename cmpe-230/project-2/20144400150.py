6e021efd5afe08f4830f4758d905a7391ceddade
Recep Deniz Aksoy <recepdeniza@gmail.com>
2017-04-27 20:16:42 +0300

    minor changes.

--- cmpe-230/project-2/20144400150.py
+++ cmpe-230/project-2/20144400150.py
@@ -26,14 +26,18 @@ def dir_hasher(filepath):
     direc_hash = ''.join(dir_hash)
     return hashlib.sha256(direc_hash.encode('utf-8')).hexdigest()
 #Given hash value, [path] dictionary, returns the duplicate list.
-def find_dups(d_list):
+def find_dups(d_list, regexx):
     rets = []
     for key,value in d_list.items():
         x = list(set(value))
+        t = []
+        for item in x:
+            if(re.search(regexx,item.split("/")[-1])):
+                t.append(item)
         #Dictionary format: {<hash_value> : [list_of_files_corresponds_to_hash_value]}
         #If list length is greater than one, then there are duplicates.
-        if(len(x)>1):
-            rets = rets + x
+        if(len(t)>1):
+            rets = rets + t
     return rets
 
 #Executes the given command on the list of directories using subprocess.
@@ -44,8 +48,8 @@ def command_execute(command,list):
     else:
         for item in list:
             cmd = command + " " + item.replace(" ","\ ")
-            output = subprocess.check_output(cmd, shell=True)#????
-            #output = subprocess.call(cmd, shell=True)#????
+            output = subprocess.check_output(cmd, shell=True)
+            #output = subprocess.call(cmd, shell=True)
             print (output)
 
 cwd = os.getcwd()       #Getting current working director
@@ -73,8 +77,10 @@ dirlist = args.dirs
 #First element is assigned to regex variable.
 regex = ""
 if dirlist[0][0] == '\"':
-    regex = dirlist.pop(0)
+    regex = dirlist.pop(0)[1:-1]
 
+if(len(dirlist)==0):
+    dirlist.append(cwd)
 #All files,directories and their hash values are stored in the below directories
 #Format: {<hash_value> : [list_of_files_corresponds_to_hash_value]}
 allfiles = {}
@@ -84,24 +90,22 @@ alldirectories = {}
 for fullpath in dirlist:
     for root, dirs, files in os.walk(fullpath, topdown=False):
         for fname in files:
-            if(re.search(regex,fname)):
-                file_path = root + "/" + fname
-                file_hash = file_hasher(file_path)
-                if file_hash in allfiles:
-                    allfiles[file_hash].append(file_path)
-                else:
-                    allfiles[file_hash] = [file_path]
+            file_path = root + "/" + fname
+            file_hash = file_hasher(file_path)
+            if file_hash in allfiles:
+                allfiles[file_hash].append(file_path)
+            else:
+                allfiles[file_hash] = [file_path]
         for dname in dirs:
-            if(re.search(regex,dname)):
-                dir_path = root + "/" + dname
-                dir_hash = dir_hasher(dir_path)
-                if dir_hash in alldirectories:
-                    alldirectories[dir_hash].append(dir_path)
-                else:
-                    alldirectories[dir_hash] = [dir_path]
+            dir_path = root + "/" + dname
+            dir_hash = dir_hasher(dir_path)
+            if dir_hash in alldirectories:
+                alldirectories[dir_hash].append(dir_path)
+            else:
+                alldirectories[dir_hash] = [dir_path]
 
 #Control for file or directory, then executing commands.
 if (args.type == 'f'):
-    command_execute(args.action, find_dups(allfiles))
+    command_execute(args.action, find_dups(allfiles,regex))
 else:
-    command_execute(args.action, find_dups(alldirectories))
+    command_execute(args.action, find_dups(alldirectories,regex))
